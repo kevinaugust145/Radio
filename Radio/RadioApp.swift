ecd63e0a9465038c06d77ce0a1f0c743686080f3
KevinHung <kevinaugust145@gmail.com>
2024-07-22 09:46:22 +0800

    Initial Commit

---
+++ Radio/RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioApp.swift
+//  Radio
+//
+//  Created by 洪宗燦 on 2024/7/22.
+//
+
+import SwiftUI
+
+@main
+struct RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RadioTests/RadioTests.swift
@@ -0,0 +1,17 @@
+//
+//  RadioTests.swift
+//  RadioTests
+//
+//  Created by 洪宗燦 on 2024/7/22.
+//
+
+import Testing
+@testable import Radio
+
+struct RadioTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
